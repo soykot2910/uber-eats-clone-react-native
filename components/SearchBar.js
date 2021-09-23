aea8267c507cc3672ee41599512cc2a0285fe5ef
soykot2910 <ashrafulislam2910@gmail.com>
2021-09-23 22:37:08 +0600

    place and yelp api added

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text } from "react-native";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import AntDesign from "react-native-vector-icons/AntDesign";
 
-export default function SearchBar() {
+export default function SearchBar({ cityHandler }) {
   return (
     <View style={{ marginTop: 15, flexDirection: "row" }}>
       <GooglePlacesAutocomplete
+        query={{ key: "AIzaSyAX1ipp4SKeX80bb9q_wroS2c4t8nRkmCI" }}
+        onPress={(data, details = null) => {
+          console.log(data.description);
+          const city = data.description.split(",")[0];
+          cityHandler(city);
+        }}
         placeholder="Search"
         styles={{
           textInput: {
             backgroundColor: "#eee",
             borderRadius: 20,
-            fontWeight: 700,
+            fontWeight: "700",
             marginTop: 7,
+            outline: "none",
           },
           textInputContainer: {
             backgroundColor: "#eee",
--- screens/Home.js
+++ screens/Home.js
@@ -1,17 +1,54 @@
-import React from "react";
-import { View, SafeAreaView } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, SafeAreaView, ScrollView } from "react-native";
 import Categories from "../components/Categories";
 import HeaderTabs from "../components/HeaderTabs";
+import RestaurantItems, {
+  localRestaurants,
+} from "../components/RestaurantItems";
 import SearchBar from "../components/SearchBar";
 
+const YELP_API_KEY =
+  "bdRJutLhFAQJ36t7b89CWjHFBU4OKzjt9wvZzcY-nkgmvTqlNMjZWV1eG7iBQ9R74SyfxRg9LWnBAkZY06BtAZAe4d2dfX-2vuX8a1l5V7foctHfX9UKEyoM5ts3YXYx";
+
 export default function Home() {
+  const [resturantData, setResturantData] = useState(localRestaurants);
+  const [city, setCity] = useState("San Francisco");
+  const [activeTab, setActiveTab] = useState("Delivery");
+
+  const getRestaurantsFromYelp = () => {
+    const yelpUrl = `https://api.yelp.com/v3/businesses/search?term=restaurants&location=${city}`;
+
+    const apiOptions = {
+      headers: {
+        Authorization: `Bearer ${YELP_API_KEY}`,
+      },
+    };
+
+    return fetch(yelpUrl, apiOptions)
+      .then((res) => res.json())
+      .then((json) =>
+        setRestaurantData(
+          json.businesses.filter((business) =>
+            business.transactions.includes(activeTab.toLowerCase())
+          )
+        )
+      );
+  };
+
+  useEffect(() => {
+    getRestaurantsFromYelp();
+  }, [city, activeTab]);
+
   return (
     <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
       <View style={{ backgroundColor: "white", padding: 15 }}>
         <HeaderTabs />
-        <SearchBar />
+        <SearchBar cityHandler={setCity} />
       </View>
-      <Categories />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Categories />
+        <RestaurantItems resturantData={resturantData} />
+      </ScrollView>
     </SafeAreaView>
   );
 }
